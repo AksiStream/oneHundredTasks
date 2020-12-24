88ff0ba762dbec278bb0d79d7ead4ffaa2dc8536
AksiStream <vanyok98@mail.ru>
2020-12-25 00:09:37 +0300

    Change calculator

---
+++ Change.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace oneHundredTasks
+{
+    public static class Change
+    {
+        private static readonly int[] CoinValues = {1, 5, 7, 10, 15};
+        private static int[] _count = new int[5];
+        public static void Calc(int change)
+        {
+            _count = new int[5];
+            var i = CoinValues.Length;
+            while (change > 0)
+            {
+                i--;
+                while (change >= CoinValues[i])
+                {
+                    change -= CoinValues[i];
+                    _count[i]++;
+                }
+            }
+
+            Print();
+        }
+
+        private static void Print()
+        {
+            for (var i = 0; i < _count.Length; i++) Console.WriteLine(_count[i] + " монет по " + CoinValues[i] + " рублей.");
+            Console.WriteLine("Всего " + _count.ToArray().Sum() + " монет.");
+        }
+    }
+}
--- OneHundredTasks.cs
+++ OneHundredTasks.cs
@@ -2,8 +2,12 @@
 {
     public static class OneHundredTasks
     {
-        private static void Main()
+        public static void Main()
         {
+            //тут сработает как надо
+            Change.Calc(35);
+            //тут выдаст пятью монетами вместо двух по 7 руб
+            Change.Calc(14);
 
         }
     }
